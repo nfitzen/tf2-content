4bd28c5621883ee88b9365c1c03f4a19dce971e0
Nathaniel Fitzenrider <16159914+nfitzen@users.noreply.github.com>
2019-01-12 23:13:02 -0500

    Fixed compile.bat for people without 7z installed

--- compile.bat
+++ compile.bat
@@ -1,5 +1,3 @@
-@echo off
-
 mkdir buildsupport
 rd /S /Q build
 mkdir build
@@ -13,7 +11,7 @@ if errorlevel 1 (
 if not exist buildsupport\7zSD.sfx (
     powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/OlegScherbakov/7zSFX/master/files/7zsd_extra_162_3888.7z -OutFile buildsupport\7zSFX.7z"
     %_7zip% e buildsupport\7zSFX.7z 7zsd_All.sfx
-    mv -f -T 7zsd_All.sfx buildsupport\7zSD.sfx
+    move 7zsd_All.sfx buildsupport\7zSD.sfx
     del buildsupport\7zSFX.7z
 )
 if not exist src\ncustomcontent (
@@ -21,13 +19,14 @@ if not exist src\ncustomcontent (
     exit
 )
 
+move 7zr.exe src\7zr.exe
 cd src
-
 rd /S /Q ncustomcontent\sound
 copy "..\README.md" "ncustomcontent\README.md"
-%_7zip% a -t7z ..\build\ncustomcontent.7z ncustomcontent
-del ncustomcontent\README.md
+%_7zip% a -t7z ..\build\ncustomcontent ncustomcontent
+move 7zr.exe ..\7zr.exe
 cd ..
+del src\ncustomcontent\README.md
 
-if not exist buildsupport\config.txt powershell -Command "Invoke-WebRequest https://github.com/nfitzen/tf2-content/buildsupport/config.txt -OutFile buildsupport\config.txt"
+if not exist buildsupport\config.txt powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/nfitzen/tf2-content/master/buildsupport/config.txt -OutFile buildsupport\config.txt"
 copy /b buildsupport\7zSD.sfx + buildsupport\config.txt + build\ncustomcontent.7z build\CustomContentInstaller.exe
